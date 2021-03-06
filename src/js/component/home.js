759f05fa0fe176c6b6a8f8fd09b981308b5c7aec
fabianchs <fabichasola@hotmail.com>
2021-03-06 02:17:50 +0000

    parte visual avance

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -6,19 +6,39 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 export function Home() {
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Riigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="mx-auto" id="anchogeneral">
+			<div className=" d-flex justify-content-center" id="anchogeneral">
+				<h1>TODO LIST</h1>
+			</div>
+
+			<div>
+				<div className="m-1" id="anchogeneral">
+					<input
+						type="text"
+						className="form-control"
+						placeholder="Inserta tu tarea"
+						aria-label="Username"
+					/>
+				</div>
+				<div className="bg-secondary m-1" id="anchogeneral">
+					<button
+						type="button"
+						className="btn btn-primary "
+						id="anchogeneral">
+						Enviar
+					</button>
+				</div>
+			</div>
+			<div className="bg-warning m-1" id="anchogeneral">
+				<ul className="list-group ">
+					<li className="list-group-item list-group-item-secondary align-items-center">
+						<button type="button" className="btn btn-danger m-2">
+							<i className="far fa-trash-alt"></i>
+						</button>
+						<h5>Aquí aparecerán tus tareas</h5>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
