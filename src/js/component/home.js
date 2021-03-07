3c5bbc0df9c611b02260274072e41fed9101771c
fabianchs <fabichasola@hotmail.com>
2021-03-07 23:03:48 +0000

    lista de tareas se ve bonito

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -4,6 +4,42 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
+let lista = [
+	<div
+		className="m-1 border  border-rounded-3 align-items-center"
+		id="anchogeneral">
+		<div className="row">
+			<div className="col-2 d-flex align-items-center">
+				<button type="button" className="btn btn-danger m-2">
+					<i className="far fa-trash-alt"></i>
+				</button>
+			</div>
+			<div className="col-10 d-flex align-items-center">
+				<p>Aqui apareceran tus tareas</p>
+			</div>
+		</div>
+	</div>,
+	<div
+		className="m-1 border  border-rounded-3 align-items-center"
+		id="anchogeneral">
+		<div className="row">
+			<div className="col-2 d-flex align-items-center">
+				<button type="button" className="btn btn-danger m-2">
+					<i className="far fa-trash-alt"></i>
+				</button>
+			</div>
+			<div className="col-10 d-flex align-items-center">
+				<p>Aqui apareceran tus tareas</p>
+			</div>
+		</div>
+	</div>
+];
+
+function Works() {
+	return lista;
+}
+
 export function Home() {
 	return (
 		<div className="mx-auto" id="anchogeneral">
@@ -20,29 +56,31 @@ export function Home() {
 						aria-label="Username"
 					/>
 				</div>
-				<div className="bg-secondary m-1" id="anchogeneral">
+				<div className="m-1" id="anchogeneral">
 					<button
 						type="button"
 						className="btn btn-primary "
 						id="anchogeneral">
-						Enviar
+						Agregar
 					</button>
 				</div>
 			</div>
-			<div className="bg-warning m-1" id="anchogeneral">
-				<ul className="list-group ">
-					<li className="list-group-item list-group-item-secondary align-items-center">
-						<div>
-							<button
-								type="button"
-								className="btn btn-danger m-2">
-								<i className="far fa-trash-alt"></i>
-							</button>
-							<p>Aquí aparecerán tus tareas</p>
-						</div>
-					</li>
-				</ul>
+
+			<div
+				className="m-1 border  border-rounded-3 align-items-center"
+				id="anchogeneral">
+				<div className="row">
+					<div className="col-2 d-flex align-items-center">
+						<button type="button" className="btn btn-danger m-2">
+							<i className="far fa-trash-alt"></i>
+						</button>
+					</div>
+					<div className="col-10 d-flex align-items-center">
+						<p>Aqui apareceran tus tareas</p>
+					</div>
+				</div>
 			</div>
+			<div>{Home()}</div>
 		</div>
 	);
 }
